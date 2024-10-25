02dc028c17c1d082fe541c9be8df26d5d7dd273a
beliyzy <62857691+beliyzy@users.noreply.github.com>
2024-10-25 23:50:53 +0300

    Time: 5 ms (99.70%) | Memory: 37.3 MB (72.52%) - LeetSync

--- 242-valid-anagram/valid-anagram.kt
+++ 242-valid-anagram/valid-anagram.kt
@@ -1,18 +1,22 @@
 class Solution {
     fun isAnagram(s: String, t: String): Boolean {
-        if (s.length != t.length) return false
-
-        val store = IntArray(26)
-
+        if (s.length != t.length) {
+            return false
+        }
+        
+        val arr = IntArray(26)
+        
         for (i in s.indices) {
-            store[s[i] - 'a']++
-            store[t[i] - 'a']--
+            arr[s[i] - 'a']++
+            arr[t[i] - 'a']--
         }
-
-        for (n in store) {
-            if (n != 0) return false
+        
+        for (n in arr) {
+            if (n != 0) {
+                return false
+            }
         }
-
+        
         return true
     }
 }
